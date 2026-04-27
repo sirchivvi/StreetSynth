6a665b7ccb99fc6ebf06d753c129ca6bc12fd242
Akhil Chivukula <akhil_1604@hotmail.com>
2026-04-27 14:49:01 +0530

    Fix gradio launch error for HuggingFace Spaces

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ os.environ["LIGHTWEIGHT"] = "1"
 
 from ui.app import demo
 
-demo.launch(server_name="0.0.0.0", server_port=7860)
+demo.launch(server_name="0.0.0.0", server_port=7860, share=False, show_error=True)
